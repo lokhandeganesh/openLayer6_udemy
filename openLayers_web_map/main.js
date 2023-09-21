3025e41b045aec286cf8cf5a70f6e1eeeebed3dd
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-09-21 17:49:48 +0530

    added interaction on map

--- openLayers_web_map/main.js
+++ openLayers_web_map/main.js
@@ -4,14 +4,16 @@ function init() {
   const Map = new ol.Map({
     view: new ol.View({
       center: [0, 0], // X,Y
-      zoom: 2
+      zoom: 2,
+      rotation: 0.5
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    target: 'js-map'
+    target: 'js-map',
+    KeyboardEventTarget: document
   })
 
   const popupContainerElement = document.getElementById('popup-coordinates');
@@ -28,6 +30,29 @@ function init() {
     popup.setPosition(undefined);
     popup.setPosition(clickedCoordinate);
     popupContainerElement.innerHTML = clickedCoordinate
+  });
 
+  // adding Interactions
+  const dragRotateInteraction = new ol.interaction.DragRotate({
+    condition: ol.events.condition.altKeyOnly
   })
+  Map.addInteraction(dragRotateInteraction);
+
+  // adding Draw interaction
+  const drawInteraction = new ol.interaction.Draw({
+    type: 'Polygon'
+  })
+  Map.addInteraction(drawInteraction);
+
+  drawInteraction.on('drawend', function (evt) {
+    // console.log(evt)
+    let parser = new ol.format.GeoJSON();
+    let drawnFeatures = parser.writeFeatures([evt.feature]); //parser.writeFeaturesObject([evt.feature]);
+    console.log(drawnFeatures);
+
+    // Map.addLayer(drawnFeatures);
+  })
+
+
+
 }
