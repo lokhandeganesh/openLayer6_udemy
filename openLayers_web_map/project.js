b2c1f37b4aa115f79b0be3237ef33e0150c48779
lokhandeganesh <lokhandeganesh8@gmail.com>
2023-10-19 16:09:50 +0530

    project completed

--- openLayers_web_map/project.js
+++ openLayers_web_map/project.js
@@ -1,12 +1,13 @@
 window.onload = init
 
 function init() {
-
+  const austrCenterCoordinates = [15091875.539375868, -2890099.0297847847];
+  const initZoom = 1;
 
   const Map = new ol.Map({
     view: new ol.View({
-      center: [15091875.539375868, -2890099.0297847847],
-      zoom: 1,
+      center: austrCenterCoordinates,
+      zoom: initZoom,
       extent: [8700313.865909653, -9512187.262171041, 21430987.92760313, 2627132.214786]
 
     }),
@@ -17,6 +18,8 @@ function init() {
     ],
     target: 'project-map'
   });
+
+  const mapView = Map.getView();
 
   // Australian Cities GeoJson
   // Vector Styling
@@ -100,7 +103,6 @@ function init() {
   Map.addLayer(austCitiesLayer);
 
   // Map Features click logic
-
   const navElements = document.querySelector('.column-navigation');
   const cityNameElement = document.getElementById('cityname');
   const cityImageElement = document.getElementById('cityimage');
@@ -110,27 +112,36 @@ function init() {
   const cityNameElement = $('#cityname');
   const cityImageElement = $('#cityimage');
  */
-  const mapView = Map.getView();
 
+  //To bind the event 
   Map.on('singleclick', function (evt) {
-    Map.forEachFeatureAtPixel(evt.pixel, (feature, layer) => {
-      // console.log(feature);
+    // Checking if feature is present then get information of it
+    let isFeatureAtPixel = Map.hasFeatureAtPixel(evt.pixel);
 
-      // Accessing feature properties
-      let featureCityName = feature.get('Cityname');
-      let featureCityImage = feature.get('Cityimage');
+    if (isFeatureAtPixel) {
+      Map.forEachFeatureAtPixel(evt.pixel, (feature, layer) => {
+        // Accessing feature properties
+        let featureCityName = feature.get('Cityname');
+        let featureCityImage = feature.get('Cityimage');
 
-      let navElement = navElements.children.namedItem(featureCityName);
-      // let navElement = $(`#${featureCityName}`)
+        let navElement = navElements.children.namedItem(featureCityName);
+        // let navElement = $(`#${featureCityName}`)
+        mainLogic(feature, navElement);
+      })
 
-      mainLogic(feature, navElement, featureCityName, featureCityImage);
-    })
+    }
+    // No feature is present on map click then go to Home Navigation.
+    else {
+      let navHomeElement = $('#Home')[0]
+      mainLogic(undefined, navHomeElement);
+    }
+
+
+
   })
 
-
-
   // Function for logic
-  function mainLogic(selFeature, clickedAnchorElement, featureCityName, featureCityImage) {
+  function mainLogic(selFeature, clickedAnchorElement) {
     // Re-assign active class to the clicked element
     let currentActiveStyledElement = document.querySelector(".active");
     // let currentActiveStyledElement = $(".active");
@@ -141,23 +152,100 @@ function init() {
     clickedAnchorElement.className = 'active';
     // clickedAnchorElement.addClass('active');
 
-    // Change view based on the feature
-    let featureCoordinates = selFeature.get('geometry').getCoordinates();
-    // console.log(featureCoordinates)
-    mapView.animate({ center: featureCoordinates }, { zoom: 5 })
-
+    // Default Style for features    
     // assigning default style to un-selected feature
     let aussieCitiesFeatures = austCitiesLayer.getSource().getFeatures();
     aussieCitiesFeatures.forEach((unSelFeature) => {
       unSelFeature.setStyle(aussieCitiesStyle);
     });
-    // Styling Selected feature
-    selFeature.setStyle(selectedFeatureStyle);
 
-    // Changing title and image of for city    
-    cityNameElement.innerHTML = `Name of the City: ${featureCityName}`;
-    cityImageElement.src = `${IMAGE_PATH}/${featureCityImage}.jpg`;
+    // Home Element : Change content in the menu to Home
+    if (clickedAnchorElement.id === 'Home') {
+      // Animate to Home
+      mapView.animate({ center: austrCenterCoordinates }, { zoom: initZoom })
+
+      // Changing title and image of for city    
+      cityNameElement.innerHTML = $('#cityname').data("original-value");
+      cityImageElement.src = $('#cityimage').data("original-value");
+    }
+    // Change view, and content in the menu based on the feature
+    else {
+      // Styling Selected feature
+      selFeature.setStyle(selectedFeatureStyle);
+      // Change view based on the feature
+      let featureCoordinates = selFeature.get('geometry').getCoordinates();
+      // console.log(featureCoordinates)
+      // Animate to feature
+      mapView.animate({ center: featureCoordinates }, { zoom: 5 })
+      // Accessing feature properties
+      let featureCityName = selFeature.get('Cityname');
+      let featureCityImage = selFeature.get('Cityimage');
+
+      // Changing title and image of for city    
+      cityNameElement.innerHTML = `Name of the City: ${featureCityName}`;
+      cityImageElement.src = `${IMAGE_PATH}/${featureCityImage}.jpg`;
+    };
 
 
-  }
+  };
+
+  // Navigation Button Logic
+  const anchorNavElements = document.querySelectorAll('.column-navigation > a');
+  for (let anchorNavElement of anchorNavElements) {
+    anchorNavElement.addEventListener('click', (evt) => {
+
+      let clickedAnchorElement = evt.currentTarget;
+      let clickedAnchorElementId = clickedAnchorElement.id;
+
+      let aussieCitiesFeatures = austCitiesLayer.getSource().getFeatures();
+
+      aussieCitiesFeatures.forEach((feature) => {
+        // console.log(feature);
+        let featureCityName = feature.get('Cityname');
+        let featureCityImage = feature.get('Cityimage');
+
+        if (clickedAnchorElementId === featureCityName) {
+          mainLogic(feature, clickedAnchorElement);
+        }
+      });
+
+      // Home Navigation Case
+      if (clickedAnchorElementId === 'Home') {
+        mainLogic(undefined, clickedAnchorElement);
+
+      }
+    });
+  };
+
+  // Feature :hover logic
+  const popoverTextElement = $('#popover-text').get(0)  //document.getElementById('popover-text');
+  const popoverTextLayer = new ol.Overlay({
+    element: popoverTextElement,
+    positioning: 'bottom-center',
+    stopEvent: false
+  });
+
+  Map.addOverlay(popoverTextLayer);
+
+  // Changing style
+  Map.on('pointermove', (evt) => {
+    let isFeatureAtPixel = Map.hasFeatureAtPixel(evt.pixel);
+    if (isFeatureAtPixel) {
+      let featureAtPixel = Map.getFeaturesAtPixel(evt.pixel);
+      let featureName = featureAtPixel[0].get('Cityname');
+      popoverTextLayer.setPosition(evt.coordinate);
+      popoverTextElement.innerHTML = featureName;
+      // Map.getViewport().style.cursor = 'pointer';
+    } else {
+      popoverTextLayer.setPosition(undefined);
+      // Map.getViewport().style.cursor = '';
+    };
+
+    // Changing cursor style for feature to 'pointer'
+    let viewPortStyle = Map.getViewport().style
+    isFeatureAtPixel ? viewPortStyle.cursor = 'pointer' : viewPortStyle.cursor = ''
+
+  });
+
+
 };
